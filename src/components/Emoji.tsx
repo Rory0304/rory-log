ea3192988724a3621755b9766b530aa642a886bb
Rory <mitty0304@naver.com>
2023-11-29 02:22:19 +0900

    fix(Emoji): remove noto color emoji

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,20 +1,11 @@
 import React, { ReactNode } from "react"
 import { Noto_Color_Emoji } from "next/font/google"
 
-const notoColorEmoji = Noto_Color_Emoji({
-  weight: ["400"],
-  subsets: ["emoji"],
-})
-
 type Props = {
   className?: string
   children?: ReactNode
 }
 
 export const Emoji = ({ className, children }: Props) => {
-  return (
-    <span className={className} css={[notoColorEmoji.style]}>
-      {children}
-    </span>
-  )
+  return <span className={className}>{children}</span>
 }
